21c6a00cc915997e80894a7eb68ea01609fdc406
Sarah Schwartz <seschwartz8@gmail.com>
2020-01-31 10:06:21 -0800

    adjust Pizza methods to be getters and setters, create displayPrice function, and set up document.ready jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,58 @@
 class Pizza {
   constructor() {
-    this.size = ""
+    this.size = "small"
     this.toppings = [];
     this.price = 0;
   }
 
-  set size(size) {
+  setSize(size) {
     this.size = size;
   }
+ 
+  getSize() {
+    return this.size;
+  }
 
-  set toppings(toppings) {
+  setToppings(toppings) {
     this.toppings.push(toppings);
   }
 
-  set price(size, toppings) {
+  getToppings() {
+    return this.toppings;
+  }
+
+  setPrice(size, toppings) {
     if (size === "small") {
-      this.price += 10;
+      this.price = 10;
     } else if (size === "medium") {
-      this.price += 12;
+      this.price = 12;
     } else if (size === "large") {
-      this.price += 15;
+      this.price = 15;
     }
     this.price += 0.5 * toppings.length;
   }
 
-
+  getPrice() {
+    return this.price;
+  }
 }
+
+function displayPrice(price) {
+  $(".price").empty();
+  $(".price").append(`<h5>Current Price: ${price}</h5>`)
+}
+
+
+// USER INTERFACE LOGIC
+$(document).ready(function(){
+  let pizza = new Pizza();
+  pizza.setPrice(pizza.getSize(), pizza.getToppings());
+  displayPrice(pizza.getPrice());
+  
+
+  $("form").submit(function(event){
+    event.preventDefault();
+
+
+  })
+})
